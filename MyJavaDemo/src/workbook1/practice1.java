44f69a25b17d3adfc137b0ef8ab87d13bcaa00d0
DELL <DELL@192.168.0.9>
2022-05-21 08:02:11 +0530

    First Commit: TestCase1 Added

---
+++ MyJavaDemo/src/workbook1/practice1.java
@@ -0,0 +1,28 @@
+package workbook1;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		//switch 
+		int a = 2;
+		switch(a){
+			case 0:
+				System.out.println("inside case0");
+					break;
+			case 1:
+				System.out.println("inside case1");
+					break;
+			case 2:
+				System.out.println("inside case2");
+					break;
+			case 3:
+				System.out.println("inside case3");
+					break;
+			default:
+				System.out.println("inside default block");
+			
+		}
+		
+	}
+
+}
