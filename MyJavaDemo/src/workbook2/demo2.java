ecf81a5af8b0634b45d70a4ef3fb31d6a60bc250
DELL <DELL@192.168.0.9>
2022-05-21 09:18:06 +0530

    Third TestCase : 3 Added

---
+++ MyJavaDemo/src/workbook2/demo2.java
@@ -0,0 +1,11 @@
+package workbook2;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("second testCase was updated...");
+		System.out.println("Third testcase was updated...");
+	}
+
+}
